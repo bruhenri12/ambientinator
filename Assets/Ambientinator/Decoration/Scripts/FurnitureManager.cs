1d9d2bee7ca97302f66a1d68aa0f346f6fcf1191
bruhenri12 <bruhenri12@hotmail.com>
2024-03-17 22:42:12 -0300

    Updating FurnitureManager

--- Assets/Ambientinator/Decoration/Scripts/FurnitureManager.cs
+++ Assets/Ambientinator/Decoration/Scripts/FurnitureManager.cs
@@ -9,6 +9,11 @@ public class Furniture
     public GameObject preview;
 }
 
+public enum InteractionType
+{
+    None, Spawn, Move
+}
+
 public class FurnitureManager : MonoBehaviour
 {
     public Furniture[] furnitures;
@@ -16,16 +21,19 @@ public class FurnitureManager : MonoBehaviour
     GameObject prefab;
     GameObject currentPreview;
 
+    InteractionType interaction = InteractionType.None;
+
     void Update()
     {
-        if (prefab && currentPreview) {
-            Ray ray = new Ray(
-                OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch),
-                OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch) * Vector3.forward
-            );
+        Debug.Log(interaction);
+        Ray ray = new Ray(
+            OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch),
+            OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch) * Vector3.forward
+        );
 
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (interaction == InteractionType.Spawn) {
                 currentPreview.transform.position = hit.point;
                 currentPreview.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
 
@@ -33,9 +41,36 @@ public class FurnitureManager : MonoBehaviour
                 {
                     Instantiate(prefab, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
                     RemovePrefab();
+                    interaction = InteractionType.None;
+                }
+            }
+
+            if (interaction == InteractionType.Move) {
+                prefab.transform.position = hit.point;
+                prefab.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+                if (OVRInput.GetDown(OVRInput.Button.One))
+                {
+                    prefab.GetComponent<Collider>().enabled = true;
+                    interaction = InteractionType.None;
+                }
+            }
+
+            if (interaction == InteractionType.None) {
+                Debug.Log(hit.transform.tag);
+                if (OVRInput.GetDown(OVRInput.RawButton.A) && hit.transform.tag == "Furniture")
+                {
+                    prefab = hit.transform.gameObject;
+                    prefab.GetComponent<Collider>().enabled = false;
+                    interaction = InteractionType.Move;
+                }
+                if (OVRInput.GetDown(OVRInput.RawButton.B) && hit.transform.tag == "Furniture")
+                {
+                    Destroy(hit.transform.gameObject);
                 }
             }
         }
+
     }
 
     public void ChangePrefab(int index)
@@ -43,9 +78,15 @@ public class FurnitureManager : MonoBehaviour
         if (furnitures.Length <= index) return;
         if (prefab && currentPreview) RemovePrefab();
         prefab = furnitures[index].prefab;
-        prefab.tag = "Furniture";
-        Destroy(currentPreview);
         currentPreview = Instantiate(furnitures[index].preview);
+        currentPreview.GetComponent<Collider>().enabled = false;
+        interaction = InteractionType.Spawn;
+    }
+
+    public void MovePrefab(GameObject selectedPrefab)
+    {
+        prefab = selectedPrefab;
+        interaction = InteractionType.Move;
     }
 
     void RemovePrefab()
